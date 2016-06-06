093dffef1f97e770ee822aacb2a05c7d8b84d959
kcoenen <coenenkris@gmail.com>
2016-06-06 11:20:08 +0200

    added install.sh and smb.conf

--- install.sh
+++ install.sh
@@ -5,25 +5,28 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-deb http://download.webmin.com/download/repository sarge contrib
+# install debendencies
+sudo deb http://download.webmin.com/download/repository sarge contrib
 cd /root
-wget http://www.webmin.com/jcameron-key.asc
-apt-key add jcameron-key.asc
+sudo wget http://www.webmin.com/jcameron-key.asc
+sudo apt-key add jcameron-key.asc
 
-sudo apt-get update && sudo apt-get install samba dosbox libre-office chromium-browser audacity filezilla konqueror lxde screen byobu sqlite3 webmin
+sudo apt-get update && sudo apt-get install samba dosbox libre-office chromium-browser audacity filezilla konqueror lxde screen byobu sqlite3 webmin -y
 
+#download youtube-dl and set output folder
 sudo wget https://yt-dl.org/downloads/2016.06.03/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
 echo "--output /media/HDD/youtube/%(title)s.%(ext)s" > ~/.config/youtube-dl.conf
 
+# create mountpoint and mount HDD or usb-stick
 echo "is external drive ntfs or fat32:"
 echo "fat32		[1]"
 echo "ntfs		[2]"
 echo "your choice:"
-read option
+read fstab
 
-case $option in
+case $fstab in
 1)
 sudo mkdir /media/HDD && sudo echo "echo /dev/sda1       /media/HDD      vfat defaults          0       0" >> /etc/fstab
 ;;
@@ -32,5 +35,13 @@ sudo mkdir /media/HDD && sudo echo "echo /dev/sda1       /media/HDD      ntfs-3g
 ;;
 esac
 
-git clone https://github.com/kcoenen/convert.git
+#set samba settings.
+
+sudo cp smb.conf /etc/samba && sudo service samba restart
+
+
+#Download convert program
+sudo mkdir /media/HDD/youtube
+
+git clone https://github.com/kcoenen/convert.git /media/HDD/youtube
 

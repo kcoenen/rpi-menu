93ca3220bedf310f8957ad2dce2aad91fa252501
kcoenen <coenenkris@gmail.com>
2016-06-11 12:34:21 +0200

    added samba config

---
+++ config_samba.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script should only be run as root"
+    exit 1
+fi
+
+if mount | grep /dev/sda > /dev/null; then
+	pass
+else
+    echo "is external drive ntfs or fat32:"
+	echo "fat32		[1]"
+	echo "ntfs		[2]"
+	echo "your choice:"
+	read fstab
+	
+	case $fstab in
+		1)
+		sudo mkdir /media/HDD && sudo mount /dev/sda1 /media/HDD && sudo echo "echo /dev/sda1       /media/HDD      vfat defaults          0       0" >> /etc/fstab
+		;;
+		2)
+		sudo mkdir /media/HDD && sudo echo "echo /dev/sda1       /media/HDD      ntfs-3g defaults          0       0" >> /etc/fstab
+		;;
+	esac
+	
+	sudo cp smb.conf /etc/samba && sudo service smbd restart
+fi
